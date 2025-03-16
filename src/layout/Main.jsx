cb54184b13eaa427c511e465c178d9149b2af85b
yKaayo <caiokaea@gmail.com>
2025-03-16 11:25:05 -0300

    Some adjustments

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Tooltip,
   TooltipTrigger,
@@ -12,23 +12,36 @@ import Recipe from "../layout/Recipe";
 export default function Main() {
   const [addIngredient, setAddIngredient] = useState([]);
   const [recipe, setRecipe] = useState("");
+  const recipeSection = useRef(null);
 
   function handleSubmit(e) {
     e.preventDefault();
-    const formData = new FormData(e.target);
+    const formData = new FormData(e.currentTarget);
     const newIngredient = formData.get("ingredient");
     setAddIngredient((ingredient) => [...ingredient, newIngredient]);
-    e.target.reset()
+    e.currentTarget.reset();
   }
-  
+
   const ingredients = addIngredient.map((ingredient) => (
     <p key={ingredient}>- {ingredient}</p>
   ));
 
   async function getRecipe() {
-    const GENERATED_RECIPE = await generateRecipe(ingredients);
+    const allIngredients = ingredients.map((ingredient) => ingredient.key);
+    const GENERATED_RECIPE = await generateRecipe(allIngredients);
     setRecipe(GENERATED_RECIPE);
   }
+
+  useEffect(() => {
+    if (recipe !== "" && recipeSection.current !== null) {
+      const yCoord =
+        recipeSection.current.getBoundingClientRect().top + window.scrollY;
+      window.scroll({
+        top: yCoord,
+        behavior: "smooth",
+      });
+    }
+  }, [recipe]);
 
   return (
     <main className="px-5 sm:px-10 min-h-[calc(100dvh_-_60px)] bg-slate-50">
@@ -51,36 +64,42 @@ export default function Main() {
           {<li className="mt-4 mb-10 text-lg">{ingredients}</li>}
         </ul>
 
-        <section className="flex justify-between items-center gap-5 md:gap-10 px-6 py-3 rounded-lg bg-gray-200">
+        <section className="grid grid-cols-1 md:grid-cols-2 justify-between items-center gap-5 md:gap-10 px-6 py-3 rounded-lg bg-gray-200">
           <div className="flex flex-col gap-2">
-            <h3 className="text-2xl font-semibold">Pronto para uma receita?</h3>
-            <p>Através dos ingredientes é gerado uma receita</p>
+            <h3 className="text-2xl font-semibold text-balance text-center md:text-start">
+              Pronto para uma receita?
+            </h3>
+            <p className="text-balance text-center md:text-start">
+              Através dos ingredientes é gerado uma receita
+            </p>
           </div>
 
-          <TooltipProvider delayDuration={300}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button
-                  onClick={getRecipe}
-                  disabled={ingredients.length > 2 ? false : true}
-                  className={`${
-                    ingredients.length > 2
-                      ? "bg-orange-500 border-orange-500 hover:text-orange-500 font-semibold hover:bg-transparent"
-                      : "bg-orange-300 border-orange-300"
-                  } px-4 py-2 rounded-lg text-white border-2 duration-300`}
+          <div className="flex md:justify-end justify-center items-center">
+            <TooltipProvider delayDuration={300}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    onClick={getRecipe}
+                    disabled={ingredients.length > 2 ? false : true}
+                    className={`${
+                      ingredients.length > 2
+                        ? "bg-orange-500 border-orange-500 hover:text-orange-500 font-semibold hover:bg-transparent"
+                        : "bg-orange-300 border-orange-300"
+                    } px-4 py-2 rounded-lg text-white border-2 duration-300`}
+                  >
+                    Gerar Receita
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent
+                  className={ingredients.length > 2 ? "hidden" : null}
                 >
-                  Gerar Receita
-                </button>
-              </TooltipTrigger>
-              <TooltipContent
-                className={ingredients.length > 2 ? "hidden" : null}
-              >
-                <p>Adicione no mínimo 3 ingredientes</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+                  <p>Adicione no mínimo 3 ingredientes</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
         </section>
-        <Recipe recipe={recipe} />
+        <Recipe recipe={recipe} ref={recipeSection} />
       </div>
     </main>
   );
--- src/layout/Recipe.jsx
+++ src/layout/Recipe.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 import ReactMarkdown from "react-markdown";
+import { forwardRef } from "react";
 
-export default function Recipe({ recipe }) {
+const Recipe = forwardRef(({ recipe }, ref) => {
   return (
-    <section className="px-5 sm:px-10 py-10">
+    <section ref={ref} className="px-5 sm:px-10 py-10" aria-live="polite">
       {recipe && <h2>Chef Virtual recomendou:</h2>}
       <ReactMarkdown>{recipe}</ReactMarkdown>
     </section>
   );
-}
+});
+
+export default Recipe;
